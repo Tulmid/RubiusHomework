c7d9e21fd16ddf0628f2443f31d71c1a69627156
Tulmid <giptardy@gmail.com>
2024-10-31 12:15:16 +0700

    Add files via upload

---
+++ Lesson1/ConsoleApp2_DZ_1/ConsoleApp2_DZ_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2_DZ_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("HEllo, i am Миша");
+            Console.WriteLine(("Мне 27 лет"));
+        }
+    }
+}
