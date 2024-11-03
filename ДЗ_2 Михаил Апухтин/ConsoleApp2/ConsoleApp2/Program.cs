c4e5728b9f402780b3987b4a403acdc55ef03b81
Tulmid <giptardy@gmail.com>
2024-11-03 23:42:35 +0700

    Add files via upload

---
+++ ДЗ_2 Михаил Апухтин/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВВЕДИТЕ ГОД: ");
+            int got = Convert.ToInt32(Console.ReadLine());
+            if (got % 4 == 0 && got % 100 != 0 || got % 400 == 0 && got % 100 != 0)
+            {
+                    Console.WriteLine("Yes");
+                
+            } else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ ДЗ_2 Михаил Апухтин/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Запрашиваем у пользователя количество расходов
+            Console.Write("Введите количество расходов: ");
+            int numExpenses = int.Parse(Console.ReadLine());
+
+            // Создаем список для хранения расходов
+            List<decimal> expenses = new List<decimal>();
+
+            // Запрашиваем у пользователя каждую статью расходов
+            for (int i = 0; i < numExpenses; i++)
+            {
+                Console.Write($"Введите сумму расхода #{i + 1}: ");
+                decimal expense = decimal.Parse(Console.ReadLine());
+                expenses.Add(expense);
+            }
+
+            // Выводим все введенные расходы
+            Console.WriteLine("\nВаши расходы:");
+            for (int i = 0; i < expenses.Count; i++)
+            {
+                Console.WriteLine($"Расход #{i + 1}: {expenses[i]:F2}");
+            }
+
+            // Суммируем и выводим общую сумму расходов
+            decimal totalExpense = 0;
+            foreach (var expense in expenses)
+            {
+                totalExpense += expense;
+            }
+            Console.WriteLine($"\nОбщая сумма расходов: {totalExpense:F2}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Пожалуйста, введите корректные числовые значения.");
+        }
+    }
+}
